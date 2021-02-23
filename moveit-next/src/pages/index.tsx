54c9df3af46c68edab1b20e87b532a6eeb0fdc2f
evenilsonlp <evenilsonlp@gmail.com>
2021-02-23 12:00:48 -0300

    AULA 02: Criando Componente Countdown + add favicon e title

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,18 +1,24 @@
-import styles from '../styles/pages/Home.module.css'
+import Head from 'next/head';
 
-import { Profile } from "../components/Profile"
+import styles from '../styles/pages/Home.module.css';
+
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from '../components/Countdown';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
-
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
       </section>
     </div>
