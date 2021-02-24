4def8b81e9c77ed4f9c0a55b19fe777bbbffb7a6
evenilsonlp <evenilsonlp@gmail.com>
2021-02-24 09:49:18 -0300

    AULA 03: Criando o Componente ChallengeBox

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -6,6 +6,7 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from '../components/Countdown';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -19,6 +20,9 @@ export default function Home() {
           <Profile />
           <CompletedChallenges />
           <Countdown />
+        </div>
+        <div>
+          <ChallengeBox />
         </div>
       </section>
     </div>
